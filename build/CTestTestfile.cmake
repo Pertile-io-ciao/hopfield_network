61a2a81dbae0ab058a2cd646d77934f85bb169e9
meggyrizzo <meggy.rizzo@gmail.com>
2025-07-27 18:57:25 +0200

    aggiunto cmake e modificato in modo che vada il programma

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marghe/hopfield_network
+# Build directory: /home/marghe/hopfield_network/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
