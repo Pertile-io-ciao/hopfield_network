3170e0c848135105fe26080447fb1836d530280b
Pertile-io-ciao <chiara.pertile777@gmail.com>
2025-07-30 19:37:05 +0200

    riorganizzazione

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marghe/hopfield_network
-# Build directory: /home/marghe/hopfield_network/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
