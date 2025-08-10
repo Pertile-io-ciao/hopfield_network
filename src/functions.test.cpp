ff9521b1a7317766d522dee2e89aa5ec083a1066
DarthEma <emanuelepio.tusa@gmail.com>
2025-08-10 17:08:11 +0200

    spostati doctest e test n src, implementazione di test di due funzioni

---
+++ src/functions.test.cpp
@@ -0,0 +1,59 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+// #include <cstdlib>
+// #include <random>
+
+#include "functions.hpp"
+
+TEST_CASE("testing the conversion from an image into a vector") {
+  // sf::Image image;
+  // std::vector<sf::Color> pixels;
+  // std::random_device r;
+  // std::default_random_engine eng{r()};
+  // std::uniform_real_distribution<sf::Uint8> dist_color{0,255};
+
+  //  int i;
+  //  sf::Color v = pixels[i];
+  //  for (i=0; i != 255; ++i) {
+  //    v = {dist_color(eng), dist_color(eng), dist_color(eng),
+  //    dist_color(eng)};
+  //  }
+
+  SUBCASE("testing the blacknwhite function") {
+    int width(12);
+    int height(10);
+    std::size_t size = (width * height);
+    std::vector<sf::Color> pixels(size, sf::Color(0, 0, 0, 0));
+    pixels[0] = {0, 0, 0, 0};          // -1
+    pixels[1] = {0, 0, 0, 100};        // 1
+    pixels[2] = {10, 10, 10, 100};     // 1
+    pixels[10] = {10, 10, 10, 255};    // 1
+    pixels[25] = {122, 39, 234, 255};  // 1
+    pixels[30] = {122, 234, 39, 255};  // -1
+    std::vector<int> result = blacknwhite(pixels);
+
+    CHECK(pixels.size() == doctest::Approx(120));
+    CHECK(result[0] == doctest::Approx(-1));
+    CHECK(result[1] == doctest::Approx(1));
+    CHECK(result[2] == doctest::Approx(1));
+    CHECK(result[10] == doctest::Approx(1));
+    CHECK(result[25] == doctest::Approx(1));
+    CHECK(result[30] == doctest::Approx(-1));
+  }
+
+  SUBCASE("testing the bilinear interpolation function") {
+    int inW{4};
+    int inH{5};
+    std::size_t insize = (inW * inH);
+    std::vector<int> input1(insize, 1);
+    std::vector<int> output1(64 * 64, 1);
+    CHECK(output1 == bilinear_interpolation(input1, inW, inH));
+    std::vector<int> input2(insize, 1);
+    input2[5] = input2[6] = input2[9] = input2[10] = input2[13] = input2[14] =
+        -1;  // immagine 4x5 bianca con bordo nero
+    std::vector<int> output2 = bilinear_interpolation(input2, inW, inH);
+    CHECK(output2[19] == 1);
+    // AGGIUNGERE ALTRI CASI
+  }
+}
