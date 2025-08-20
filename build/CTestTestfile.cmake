58725f04e229e44b958275057450b666fa2b36b2
DarthEma <emanuelepio.tusa@gmail.com>
2025-08-20 17:55:25 +0200

    .

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,3 +4,15 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test([=[hopfield_network.t]=] "/home/ubuntu/hopfield_network/hopfield_network/build/Debug/hopfield_network.t")
+  set_tests_properties([=[hopfield_network.t]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/hopfield_network/hopfield_network/CMakeLists.txt;56;add_test;/home/ubuntu/hopfield_network/hopfield_network/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test([=[hopfield_network.t]=] "/home/ubuntu/hopfield_network/hopfield_network/build/Release/hopfield_network.t")
+  set_tests_properties([=[hopfield_network.t]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/hopfield_network/hopfield_network/CMakeLists.txt;56;add_test;/home/ubuntu/hopfield_network/hopfield_network/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test([=[hopfield_network.t]=] "/home/ubuntu/hopfield_network/hopfield_network/build/RelWithDebInfo/hopfield_network.t")
+  set_tests_properties([=[hopfield_network.t]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ubuntu/hopfield_network/hopfield_network/CMakeLists.txt;56;add_test;/home/ubuntu/hopfield_network/hopfield_network/CMakeLists.txt;0;")
+else()
+  add_test([=[hopfield_network.t]=] NOT_AVAILABLE)
+endif()
