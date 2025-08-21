6144fcfd324df8a54b95b4c51c63b4fa2fbb10b2
meggyrizzo <meggy.rizzo@gmail.com>
2025-08-21 13:24:18 +0200

    cambiato gitignore

--- build/DartConfiguration.tcl
+++ build/DartConfiguration.tcl
@@ -4,14 +4,14 @@
 
 
 # Configuration directories and files
-SourceDirectory: /home/ubuntu/hopfield_network/hopfield_network
-BuildDirectory: /home/ubuntu/hopfield_network/hopfield_network/build
+SourceDirectory: /home/marghe/hopfield_network
+BuildDirectory: /home/marghe/hopfield_network/build
 
 # Where to place the cost data store
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: Emanuele
+Site: THINKBOOK2
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Linux-c++
@@ -27,7 +27,7 @@ SubmitInactivityTimeout:
 NightlyStartTime: 00:00:00 EDT
 
 # Commands for the build/test/submit cycle
-ConfigureCommand: "/usr/bin/cmake" "/home/ubuntu/hopfield_network/hopfield_network"
+ConfigureCommand: "/usr/bin/cmake" "/home/marghe/hopfield_network"
 MakeCommand: /usr/bin/cmake --build . --config "${CTEST_CONFIGURATION_TYPE}"
 DefaultCTestConfigurationType: Release
 
