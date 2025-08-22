a5e9a481bb377de0327b94b4d1fb7627bbe1acd3
Pertile-io-ciao <chiara.pertile777@gmail.com>
2025-08-22 21:39:01 +0200

    hebb

--- src/hebb.hpp
+++ src/hebb.hpp
@@ -11,13 +11,16 @@ namespace hp {
    public:
     Hebb(std::string source, std::string destination);
     void process();
+    void save_matrix(const std::vector<std::vector<float>>& matrix) const;
 
    private:
     std::string sourceFolder;
     std::string destinationFolder;
 
     // spostata qui per non ridefinirla dentro process()
-    void save_matrix(const std::vector<std::vector<float>>& matrix) const;
+    //void save_matrix(const std::vector<std::vector<float>>& matrix) const;
+     // dichiarazione friend: permette al test di accedere ai membri privati
+    //friend void test_save_matrix_accessible();
   };
 }
 #endif
